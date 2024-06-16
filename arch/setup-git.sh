e09108558e01ec7f2f9a7d33607a2a5bcbf415b7
Hannele Ruiz <justlemoncl@gmail.com>
2024-06-16 03:50:16 -0400

    Added installation step

--- arch/setup-git.sh
+++ arch/setup-git.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Packages
+pacman -Sy git gnupg --needed --noconfirm
+paru -S git-credential-manager git-credential-manager-extras --noconfirm
+
 # Basics
 git config --global user.email "justlemoncl@gmail.com"
 git config --global user.name "Hannele Ruiz"
 git config --global init.defaultBranch master
 
 # Git Credential Manager
-paru -S git-credential-manager git-credential-manager-extras
 git config --global credential.credentialStore secretservice
 git-credential-manager configure
 
